e65692af6aa0c9f9fbc2b91c74ac228e1c1f5638
Saki Salman <95974163+SakiSalman@users.noreply.github.com>
2023-05-10 02:35:51 +0600

    custom document ready

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = {
-//   nex
-// }
-
-
 module.exports = {
   env : {
     DB_LOCAL_URI : 'mongodb://127.0.0.1:27017/bookit'
